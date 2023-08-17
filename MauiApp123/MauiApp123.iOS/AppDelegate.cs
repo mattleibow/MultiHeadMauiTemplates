e1626700f73888bb1b22d76836765e109601873a
Matthew Leibowitz <mattleibow@live.com>
2023-08-17 19:01:50 +0200

    Fix iOS

--- MauiApp123/MauiApp123.iOS/AppDelegate.cs
+++ MauiApp123/MauiApp123.iOS/AppDelegate.cs
@@ -14,7 +14,7 @@ public class AppDelegate : UIApplicationDelegate
         var vc = new UIViewController();
         vc.View!.AddSubview(new UILabel(Window!.Frame)
         {
-            BackgroundColor = UIColor.SystemBackground,
+            BackgroundColor = UIColor.White,
             TextAlignment = UITextAlignment.Center,
             Text = "Hello, iOS!",
             AutoresizingMask = UIViewAutoresizing.All,
